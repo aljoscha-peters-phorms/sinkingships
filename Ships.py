b387da9a7629ad7c466a087bbe8094ff498fa5da
L0renz00 <cornelius.lechner@gmail.com>
2022-05-10 11:44:05 +0200

    Ships Klasse umgesetzt
    Ships Klasse umgesetzt, mit copy import, getter und setter, schiff typen dictionary

---
+++ Ships.py
@@ -0,0 +1,50 @@
+import copy
+
+schiff_Typen = {
+	"Fregatte" : 2,
+	"Submarine" : 2,
+	"Destroyer" : 3,
+	"Battleship" : 4,
+	"Carrier" : 5
+}
+
+class Ships():
+
+	def __init__(self, schiffTypen : schiff_Typen):
+		self.id = 0
+		self.name = ""
+		self.versenkt = False
+		self.stellen = []
+		for i in range(schiff_Typen[schiffTypen]):
+			self.stellen.append(False)
+		self.zeichen = ""
+
+	def setID(self, newID):
+		self.id = newID
+
+	def getID(self):
+		return self.id
+
+	def setVersenkt(self, newVersenkt):
+		self.versenkt = True
+
+	def isVersenkt(self):
+		return self.versenkt
+
+	def setStellen(self, newStellen):
+		self.stellen = copy.copy(newstellen)
+
+	def getStellen(self):
+		return self.stellen
+
+	def setStelle(self, pos, value):
+		self.stellen[pos] = value
+
+	def getStelle(self, pos):
+		return self.stelle[pos]
+
+	def setZeichen(self, newValue):
+		self.zeichen = newValue
+
+	def getZeichen(self):
+		return self.zeichen
