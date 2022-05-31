476f12ac9a48724dca0bb80445cff174102ee6da
MichaHaegendas <michahagen205@gmail.com>
2022-05-31 11:21:04 +0200

    lkasjdfkl

--- Intro.py
+++ Intro.py
@@ -1,86 +1,92 @@
-"""
+import os
 import curses
-import time
 from curses import wrapper
 
-def main(stdscr):
-    # Clear screen
-    
-    curses.init_pair(1,curses.COLOR_BLUE, curses.COLOR_YELLOW)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_GREEN)
-    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
-    BLUE_AND_YELLOW = curses.color_pair(1)
-    RED_AND_GREEN = curses.color_pair(2)
-    blue_and_white = curses.color_pair(3)
 
-    counter_win = curses.newwin(1,20,10,10)
-    
-    for i in range(100):
-        counter_win.clear()
-        color = blue_and_white
-        if i % 2 == 0:
-            color = BLUE_AND_YELLOW
-        counter_win.addstr(f'Count: {i}', color)
-        counter_win.refresh()
-        time.sleep(0.1)
-    stdscr.getch()
 
-wrapper(main)
-"""
+from time import sleep
 
-n = 0
 
-myString1 ="                                //////////                                     "
 
-myString2 ="                               .//////////                                     "
 
-myString3 ="                                  ###*                                         "
 
-myString4 ="                            /////////////////,                                 "
+def clearScreen():
+	os.system("cls") if os.name == 'nt' else os.system("clear")
+n = 60
 
-myString5="                            /////////////////,                                 "
+myString1 ="                                //////////                                      "
 
-myString6="           .           ,,,,,,,,,,**###/******,                                 " 
+myString2 ="                               .//////////                                       "
 
-myString7="          #(((((((#*.///////////(((((((((((((*                     ,/##(       "
+myString3 ="                                  ###*                                           "
 
-myString8="               ,/#(/////#%%%///((((//////////////////////*#(((((((((((#(       "
+myString4 ="                            /////////////////,                                   "
 
-myString9="                 ,/////////////((((/////#%%%///#%%%////////((*.                "
+myString5="                            /////////////////,                                   "
 
-myString10="                 ,/////////////((((/////#%%%///%%%%//////////                  " 
+myString6="           .           ,,,,,,,,,,**###/******,                                   " 
 
-myString11="                   ((((((((((((((((//////////////////////////.                 " 
+myString7="          #(((((((#*.///////////(((((((((((((*                     ,/##(         "
+
+myString8="               ,/#(/////#%%%///((((//////////////////////*#(((((((((((#(         "
+
+myString9="                 ,/////////////((((/////#%%%///#%%%////////((*.                  "
+
+myString10="                 ,/////////////((((/////#%%%///%%%%//////////                    " 
+
+myString11="                   ((((((((((((((((//////////////////////////.                   " 
 
 myString12="       ##################################################################(     "
 
 myString13="       ##################################################%%%%%###########*     " 
 
-myString14="         /##############################################%&&&&&&######%         "
+myString14="         /##############################################%&&&&&&######%........."
 
-myString15="          %########################################################.           "
+myString15="..........%########################################################............"
 
-myString16="          *######################################################.             "
+myString16="...........*######################################################.............."
 
-print(myString1[:n])
-print(myString2[:n])
-print(myString3[:n])
-print(myString4[:n])
-print(myString5[:n])
-print(myString6[:n])
-print(myString7[:n])
-print(myString8[:n])
-print(myString9[:n])
-print(myString10[:n])
-print(myString11[:n])
-print(myString12[:n])
-print(myString13[:n])
-print(myString14[:n])
-print(myString15[:n])
-print(myString16[:n])
-	
+def main(stdscr):
+    
+    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_BLUE)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    BLACK_AND_BLUE = curses.color_pair(1)
+    BLACK_AND_WHITE = curses.color_pair(2)
+    BLUE_AND_WHITE = curses.color_pair(3)
+    
+    stdscr.addstr( myString1 , BLUE_AND_WHITE)
 
 
+wrapper(main)
+
+
+
+
+
+
+while n > 0:
+	clearScreen()	
+	print("\033[1;30;47m " + myString1[n:])
+	print(myString2[n:])
+	print(myString3[n:])
+	print(myString4[n:])
+	print(myString5[n:])
+	print(myString6[n:])
+	print(myString7[n:])
+	print(myString8[n:])
+	print(myString9[n:])
+	print(myString10[n:])
+	print(myString11[n:])
+	print("\033[1;37;44m " + myString12[n:])
+	print(myString13[n:])
+	print(myString14[n:])
+	print(myString15[n:])
+	print(myString16[n:])
+	sleep(0.1)
+
+
+	n = n - 1
 
 
 
