407060273d7428f2f155aa69ba27fc69cfa18dac
MichaHaegendas <michahagen205@gmail.com>
2022-06-12 17:12:32 +0200

    new menu

---
+++ menu.py
@@ -0,0 +1,13 @@
+#menu.py
+
+print("welcome to sinking ships")
+
+
+input1 = input("type OK if you want to start playing" + "\n" )
+
+
+
+if input1 == "OK":
+	print("have fun")
+else:
+	print("ok bye")
