fc0522d49c9bb497d3729ef9725db079424aaa9e
L0renz00 <cornelius.lechner@gmail.com>
2022-06-13 07:17:37 +0200

    Main game file set frame

--- main.py
+++ main.py
@@ -2,9 +2,32 @@ import Player
 import menu
 
 def gameLoop():
+    global done
     done = False
-   
-    while not done:
-         menu.main()
+
+    menu.main()
     
+    p1 = Player.Player()
+    p1.setOwnMap()
+    p1.setEnemyMap()
+
+    p2 = Player.Player()
+    p2.setOwnMap()
+    p2.setEnemyMap()
+
+    p1.askCheckPlaceShips()
+    p2.askCheckPlaceShips()
+    
+    while not done:
+        while p1.shoot():
+            p1.shoot()
+        while p2.shoot():
+            p2.shoot()
+        
+        if checkDone():
+            done == True
+    
+    scoreScreen()
+    end()
+
 gameLoop()
